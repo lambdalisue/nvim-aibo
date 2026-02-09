6493d5e0bd01fefbc00e3c43df45a83a02af2007
Alisue <lambdalisue@gmail.com>
2026-02-10 01:12:29 +0900

    feat: add AiboPromptNormal and AiboPromptBorder highlight groups

--- plugin/aibo.lua
+++ plugin/aibo.lua
@@ -8,6 +8,21 @@ if vim.fn.has("nvim-0.10.0") ~= 1 then
   return
 end
 
+-- Define custom highlight groups for prompt window
+local function setup_highlights()
+  vim.api.nvim_set_hl(0, "AiboPromptNormal", { default = true, link = "Normal" })
+  vim.api.nvim_set_hl(0, "AiboPromptBorder", { default = true, link = "FloatBorder" })
+end
+
+-- Setup highlights on startup
+setup_highlights()
+
+-- Re-setup highlights when colorscheme changes
+vim.api.nvim_create_autocmd("ColorScheme", {
+  group = vim.api.nvim_create_augroup("aibo_highlights", { clear = true }),
+  callback = setup_highlights,
+})
+
 -- Initialize command modules
 require("aibo.command.aibo").setup()
 require("aibo.command.aibo_send").setup()
